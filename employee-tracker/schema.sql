c294b1d811bef356048dc480bba5b4765ba45c65
oglesby80 <OglesbyRodriguez@gmail.com>
2024-09-09 12:51:24 -0400

    2nd push

---
+++ employee-tracker/schema.sql
@@ -0,0 +1,28 @@
+-- schema.sql
+
+CREATE DATABASE company;
+
+\c company;
+
+-- Table for Departments
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- Table for Roles
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL REFERENCES department(id)
+);
+
+-- Table for Employees
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id)
+);
